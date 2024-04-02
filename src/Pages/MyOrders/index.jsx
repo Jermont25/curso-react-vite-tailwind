2737bf595de6596c6658d64925e088828caaa0cc
Jerson Uribe <jerson.jhum@gmail.com>
2024-04-01 22:12:00 -0500

    Enrutación de las páginas, componente Navbar, componente Layout, componente card

---
+++ src/Pages/MyOrders/index.jsx
@@ -0,0 +1,12 @@
+import Layout from '../../Components/Layout'
+
+function MyOrders() {
+
+    return (
+      <Layout>
+        My MyOrders
+      </Layout>
+    )
+  }
+  
+  export default MyOrders
