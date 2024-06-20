ffd45816ebb5e492045739211c9ae9cc1537c211
Jerson Uribe <jerson.jhum@gmail.com>
2024-06-19 21:52:08 -0500

    Se corrige URL base

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: mode === "development" ? "/" : "/curso-react-vite-tailwind/", // Ajusta la base URL dependiendo del entorno
+    base: "/",
   };
 });
